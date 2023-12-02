ddda54c8bb2f83b8f113d91310d6d83436085b9f
A00473252 <Subhiksha.Ramasubramanian@smu.ca>
2023-12-02 15:11:04 -0400

    Membership Model created

---
+++ Models/Membership.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FitHub.Models
+{
+    public class Membership
+    {
+        [Key]
+        public string MembershipID { get; set; }
+
+        /*[ForeignKey("User")]
+        public string UserID { get; set; }
+
+        public virtual ApplicationUser User { get; set; }*/
+
+        [Required]
+        public string MembershipType { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Amount Paid")]
+        public string AmountPaid { get; set; }
+    }
+}
