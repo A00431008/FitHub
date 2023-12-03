50fccf0eccad1b32080e2c2448d03913b3da0d82
A00473252 <Subhiksha.Ramasubramanian@smu.ca>
2023-12-03 07:09:06 -0400

    Database connection added

--- Models/Membership.cs
+++ Models/Membership.cs
@@ -8,12 +8,13 @@ namespace FitHub.Models
     public class Membership
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string MembershipID { get; set; }
 
-        /*[ForeignKey("User")]
+        [ForeignKey("User")]
         public string UserID { get; set; }
 
-        public virtual ApplicationUser User { get; set; }*/
+        public virtual User User { get; set; }
 
         [Required]
         public string MembershipType { get; set; }
@@ -23,10 +24,32 @@ namespace FitHub.Models
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
-        [Display(Name = "End Date")]
-        [DataType(DataType.Date)]
+        [Required]
+        [Display(Name = "End Date"), DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
 
+        /*public void CalculateEndDate()
+        {
+            if (StartDate != null && DurationMonths > 0)
+            {
+                EndDate = StartDate.AddMonths(DurationMonths);
+            }
+            else
+            {
+                // Handle error or default behavior when StartDate or Duration is invalid
+                // For example:
+                // throw new InvalidOperationException("Invalid Start Date or Duration");
+                // or set a default EndDate
+                // EndDate = DateTime.Now; // Set it to current date/time
+            }
+        }
+         * 
+         * You can call this method whenever you want to update the EndDate. 
+         * For instance, in your controller, when creating or updating a 
+         * Membership object, you can set the StartDate and DurationMonths, 
+         * and then call CalculateEndDate() to compute the EndDate.*/
+
+        [Required]
         [Display(Name = "Amount Paid")]
         public string AmountPaid { get; set; }
     }
---
+++ Models/MembershipDetail.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitHub.Models
+{
+    public class MembershipDetail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string MembershipTypeID { get; set; }
+
+        [Required]
+        public string MembershipTypeName { get; set; }
+
+        [Required]
+        [Display(Name = "Duration (in months)")] 
+        public int DurationMonths { get; set; }
+
+        [Required]
+        public decimal Cost { get; set;}
+
+        [Required]
+        public string Description { get; set;}
+    }
+}
