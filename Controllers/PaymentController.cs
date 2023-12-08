6c7d51a885063f1402a361a7d69f3551abf7a224
A00474480 <sivleen.kaur@smu.ca>
2023-12-07 20:06:42 -0400

    Integrated payment with booking logic
    Integrated payment with booking logic

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,28 +1,73 @@
-﻿using FitHub.Models;
+﻿using FitHub.Data;
+using FitHub.Models;
+using FitHub.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace FitHub.Controllers
 {
     public class PaymentController : Controller
     {
-        [HttpGet]
+
+        private readonly GymDbContext _context;
+        private readonly AmenityManagementService _amenityManagementService;
+
+        public PaymentController(GymDbContext context, AmenityManagementService amenityManagementService)
+        {
+            _context = context;
+            _amenityManagementService = amenityManagementService;
+        }
+
+        //[HttpGet]
         public IActionResult PaymentForm()
         {
-            // Declare and return an empty payment method form at the beginning
+            var booking = JsonConvert.DeserializeObject<Booking>(TempData["BookingData"] as String);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View("PaymentForm", new PaymentMethod { Booking = booking });
+            /*// Declare and return an empty payment method form at the beginning
             var paymentMethod = new PaymentMethod();
-            return View(paymentMethod);
+            return View(paymentMethod);*/
         }
 
         [HttpPost]
         /*[Route("/api/payment/process")]*/
         public IActionResult ProcessPayment(PaymentMethod paymentMethod)
         {
+
+            ModelState.Remove("Booking.User");
+            ModelState.Remove("Booking.UserID");
+            ModelState.Remove("Booking.Amenity");
+            ModelState.Remove("Booking.AmenityID");
+            ModelState.Remove("Booking.BookingID");
+            ModelState.Remove("Booking.AmountPaid");
+            ModelState.Remove("Booking.BookingDate");
+
+            /*string userId = paymentMethod.Booking.UserID;
+            string amenityId = paymentMethod.Booking.AmenityID;
+
+            User user = _context.User.FindAsync(userId);
+            Amenity amenity = _context.Amenity.FindAsync(amenityId);
+
+            paymentMethod.Booking.User = user;
+            paymentMethod.Booking.Amenity = amenity;*/
+
             // Always set the payment to success for this project
             if (ModelState.IsValid)
             {
-                return Ok(new { Success = true, Message = "Payment Successful !!!" });
+                _context.Booking.Add(paymentMethod.Booking);
+                _context.SaveChanges();
+                //return Ok(new { Success = true, Message = "Payment Successful !!!" });
+                return RedirectToAction("Index", "Bookings");
             }
-            
+
+
+
             return View("PaymentForm", paymentMethod);
         }
     }
